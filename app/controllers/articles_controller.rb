21880c4402d80b09d6425c3c2bd896bb5177658e
usmansohee <uxmansohee@gmail.com>
2022-08-12 11:09:47 +0500

    minor changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,20 +27,14 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    puts "create"
     @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "created successfully"
+      redirect_to @article, notice: "created successfully"
+    else
 
-    puts "usman"
-    print @article.inspect
-    puts "ahmed"
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created.." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+      puts @article.errors.full_messages
+      render "new", status: :unprocessable_entity
     end
   end
 
